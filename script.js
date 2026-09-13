4ff4bedff341195f6581041278622feae59ba597
tristantamani <tristanandreitamani@gmail.com>
2026-09-13 10:26:53 +0800

    Write logic to get computer choice

---
+++ script.js
@@ -0,0 +1,24 @@
+// Get input choice from computer
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3) + 1;
+    
+    switch(choice) {
+        case 1:
+            return "rock";
+            break;
+        case 2:
+            return "paper";
+            break;
+        case 3:
+            return "scissors";
+            break;
+    }
+}
+
+// Get input choice from user
+
+// Initialize score variable for human and computer
+
+// Write a function to play a single round
+
+// Write a function to play a single game of 5 rounds
