1b113b3a1b0bc7fa09921ce9afbe7033b4219428
tristantamani <tristanandreitamani@gmail.com>
2026-09-13 11:09:54 +0800

    Add console log to show computerChoice

--- script.js
+++ script.js
@@ -23,32 +23,32 @@ function playRound(humanChoice, computerChoice) {
     if (humanChoice === "rock") {
         if (computerChoice === "paper") {
             computerScore += 1;
-            return console.log("You lose! Paper beats rock.");
+            return console.log("Computer picked " + computerChoice + "!\nYou lose! Paper beats rock.");
         } else if (computerChoice === "scissors") {
             humanScore += 1;
-            return console.log("You win! Rock beats scissors.");
+            return console.log("Computer picked " + computerChoice + "!\nYou win! Rock beats scissors.");
         } else {
-            return console.log("No one wins, it's a tie!");
+            return console.log("Computer picked " + computerChoice + "!\nNo one wins, it's a tie!");
         }
     } else if (humanChoice === "paper") {
         if (computerChoice === "scissors") {
             computerScore += 1;
-            return console.log("You lose! Scissors beats paper.");
+            return console.log("Computer picked " + computerChoice + "!\nYou lose! Scissors beats paper.");
         } else if (computerChoice === "rock") {
             humanScore += 1;
-            return console.log("You win! Paper beats rock.");
+            return console.log("Computer picked " + computerChoice + "!\nYou win! Paper beats rock.");
         } else {
-            return console.log("No one wins, it's a tie!");
+            return console.log("Computer picked " + computerChoice + "!\nNo one wins, it's a tie!");
         }
     } else if (humanChoice === "scissors") {
         if (computerChoice === "rock") {
             computerScore += 1;
-            return console.log("You lose! Rock beats scissors.");
+            return console.log("Computer picked " + computerChoice + "!\nYou lose! Rock beats scissors.");
         } else if (computerChoice === "paper") {
             humanScore += 1;
-            return console.log("You win! Scissors beats paper.");
+            return console.log("Computer picked " + computerChoice + "!\nYou win! Scissors beats paper.");
         } else {
-            return console.log("No one wins, it's a tie!");
+            return console.log("Computer picked " + computerChoice + "!\nNo one wins, it's a tie!");
         }
     }
 }
